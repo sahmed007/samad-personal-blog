065af8724694c03a4fee9768c12b6ac19125f9e7
Samad Ahmed <samadahmed2019@gmail.com>
2022-11-26 16:27:17 -0600

    finish about page

--- layouts/AuthorLayout.js
+++ layouts/AuthorLayout.js
@@ -59,44 +59,117 @@ export default function AuthorLayout({ frontMatter }) {
               />
             </div>
           </div>
-          <div className="prose max-w-none pt-8 pb-8 dark:prose-dark xl:col-span-2">
+          <div className="prose max-w-none pt-8 pb-8 text-sm dark:prose-dark xl:col-span-2">
             <p>
-              Tails Azimuth is a professor of atmospheric sciences at the
-              Stanford AI Lab. His research interests includes complexity
-              modelling of tailwinds, headwinds and crosswinds.
+              I am a software engineer and indie hacker focused on building
+              products and tools for Web 3.0. I love talking about emerging
+              architectures, next-generation tooling, data harvesting, swarms,
+              and semantic spaces.
             </p>
             <p>
-              Tails Azimuth is a professor of atmospheric sciences at the
-              Stanford AI Lab.
+              I've used way too many tools, technologies, and languages that can
+              be listed here. I like working with opinionated frameworks and
+              strongly-typed, compiled languages such as{' '}
+              <span className="font-bold">Go</span> and{' '}
+              <span className="font-bold">Rust</span>.
+            </p>
+            <p>
+              I'm pretty comfortable when it comes to{' '}
+              <span className="font-bold">software design</span> (designing
+              cloud architectures, setting up infrastructure) and{' '}
+              <span className="font-bold">data ingestion</span> (event-driven
+              processes, pipelines, efficient storage solutions).
+            </p>
+            <p>
+              Life isn't linear. Up until this point, I have done a ton of
+              exploration and made a ton of mistakes. Within those excursions, I
+              have had some sprinkles of success and small wins.
             </p>
             <h2 id="timeline">Timeline</h2>
             <h3 id="2022">2022</h3>
             <ul>
-              <li>x</li>
-              <li>y</li>
-              <li>z</li>
+              <li>
+                Earned my{' '}
+                <span className="font-bold">
+                  AWS Certified Solutions Architect - Associate
+                </span>{' '}
+                certification
+              </li>
+              <li>
+                Awarded{' '}
+                <span className="font-bold">
+                  1st Place at Incubate Galveston + the Ion Hackathon
+                </span>{' '}
+                for pitching an AR gaming application built with location-aware
+                token dropping
+              </li>
+              <li>
+                <span className="font-bold">
+                  Dropped out of the MS Computer Science
+                </span>{' '}
+                program from Georgia Tech to pursue entrepreneurship
+              </li>
             </ul>
             <h3 id="2021">2021</h3>
             <ul>
-              <li>x</li>
-              <li>y</li>
-              <li>z</li>
+              <li>
+                <span className="font-bold">Recharged my entire life</span> by
+                spending majority of my time partying, traveling, and having as
+                much fun as I can
+              </li>
             </ul>
-            <h3 id="2021">2020</h3>
+            <h3 id="2020">2020</h3>
             <ul>
-              <li>x</li>
-              <li>y</li>
-              <li>z</li>
+              <li>
+                Earned my{' '}
+                <span className="font-bold">
+                  AWS Certified Developer - Associate
+                </span>{' '}
+                certification
+              </li>
+              <li>
+                Quit my comfy job to give a shot at freelancing and{' '}
+                <span className="font-bold">failed miserably</span>
+              </li>
             </ul>
-            <h3 id="2021">2019</h3>
+            <h3 id="2019">2019</h3>
             <ul>
-              <li>x</li>
-              <li>y</li>
-              <li>z</li>
+              <li>
+                Made a tough decision to{' '}
+                <span className="font-bold">drop my pursuit</span> of a
+                mathematical research career in academia
+              </li>
+            </ul>
+            <h3 id="2018">2018</h3>
+            <ul>
+              <li>
+                Gave a talk about{' '}
+                <span className="font-bold">
+                  Dynamic Coverage in Sensor Networks
+                </span>{' '}
+                at Lousiana State University
+              </li>
+              <li>
+                Awarded <span className="font-bold">TSU Hackathon Winner</span>{' '}
+                for Roku's "Connecting People to People" Challenge
+              </li>
+            </ul>
+            <h3 id="2017">2017</h3>
+            <ul>
+              <li>
+                Gave a talk about{' '}
+                <span className="font-bold">
+                  Homological Criteria for Coverage in Sensor Networks
+                </span>{' '}
+                at the University of Houston
+              </li>
             </ul>
             <h3 id="1996">1996</h3>
             <ul>
-              <li>x</li>
+              <li>
+                Started the exciting journey known as life in{' '}
+                <span className="font-bold">Houston, TX</span>
+              </li>
             </ul>
           </div>
         </div>
